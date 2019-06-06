53b26f6c1f9e5f77b6efd84335c9b6d27128900e
Jake Cahill <jake@ccstechme.com>
2019-06-06 16:48:52 -0400

    added test_violation checking

---
+++ tests/test_violations/test_definition_order.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import re
+import inspect
+
+def _get_sorted_classes(classes):
+    sorted_by_code = sorted(classes, key=lambda cl: cl.code)
+    sorted_by_source = sorted(
+        classes,
+        key=lambda cl: inspect.findsource(cl)[1]
+    )
+    return sorted_by_code, sorted_by_source
+
+
+def test_violation_source_order(all_module_violations):
+    """Used to force violaion order inside the source code"""
+    for _, classes in all_module_violations.items():
+        sorted_by_code, sorted_by_source = _get_sorted_classes(classes)
+        
+        assert sorted_by_code, sorted_by_source
+
+def test_violation_autoclass_order(all_module_violations):
+    """Used to force violation order inside `autoclass` directives"""
+    for module, classes in all_module_violations.items():
+        sorted_by_code, _ = _get_sorted_classes(classes)
+        pattern = re.compile(r'\.\.\sautoclass::\s(w+)')
+        sorted_by_autoclass = pattern.findall(module.__doc__)
+        sorted_by_code = [cl.__qualname__ for cl in sorted_by_code]
+
+        assert sorted_by_code == sorted_by_autoclass
