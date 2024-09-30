1fecd63b9ee2ddf4c7459b448b261e13666540f8
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2024-09-30 17:50:36 +0300

    Remove extra test fixture, create temp file in test instead

--- tests/test_cli/test_lint_command.py
+++ tests/test_cli/test_lint_command.py
@@ -1,3 +1,5 @@
+import os
+
 import subprocess
 from collections.abc import Callable
 
@@ -82,15 +84,20 @@ def test_lint_wrong_fixture(fixture_path, all_violations):
         assert str(violation_class.code) in stderr
 
 
-def test_lint_wrong_eol(fixture_path: Callable) -> None:
+def test_lint_wrong_eol(fixture_path: Callable[[str], str]) -> None:
     """Checks that `lint` command works for with with CRLF end-of-line."""
+    temp_file_path = fixture_path('.env.temp')
+    with open(temp_file_path, mode='w') as temp_file:
+        _ = temp_file.write("VARIABLE_WITH_CRLF_EOL=123\r\n")
+
     process = subprocess.Popen(
         [
             'dotenv-linter',
-            fixture_path('.env.eol'),
+            fixture_path(temp_file_path),
         ],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
+        # It is important to set this to `False`, otherwise eol are normalized
         universal_newlines=False,
         encoding='utf8',
     )
@@ -99,3 +106,5 @@ def test_lint_wrong_eol(fixture_path: Callable) -> None:
     assert process.returncode == 1
 
     assert str(InvalidEOLViolation.code) in stderr
+
+    os.remove(temp_file_path)
